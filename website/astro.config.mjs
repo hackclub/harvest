9fb650ebff09f4b77cc05ea42036c9aff34b83a0
Cheru Berhanu <cheru@cheru.dev>
2023-10-04 21:35:17 -0400

    oop! fix redirect

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -3,8 +3,6 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     redirects: {
-        '/signup': {
-            destination: 'https://forms.hackclub.com/t/x1vbYvqUqtus',
-        }
+        '/signup':'https://forms.hackclub.com/t/x1vbYvqUqtus'
     }
 });
