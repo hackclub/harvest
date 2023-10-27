15eb425bebabc82baf91d2a9bf1710be139a59af
Cheru Berhanu <cheru@cheru.dev>
2023-10-27 19:06:17 +0000

    initial dinner website

--- dinner-website/astro.config.mjs
+++ dinner-website/astro.config.mjs
@@ -1,8 +1,8 @@
 import { defineConfig } from 'astro/config';
-import nodejs from '@astrojs/node';
+import vercelServerless from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
     output: 'server',
-    adapter: nodejs({mode: "middleware"})
+    adapter: vercelServerless()
 });
